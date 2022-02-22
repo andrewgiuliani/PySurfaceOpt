f2319492328cab295d693223f6ba114bfd7ae630
Andrew Giuliani <giuliani@cims.nyu.edu>
2022-02-22 16:08:41 -0500

    edits to setup

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
-from setuptools import setup, Extension
-from setuptools.command.build_ext import build_ext
+from setuptools import setup
 import sys
 import os
-import setuptools
 
 __version__ = '0.0.1'
 
 setup(
     name='PySurfaceOpt',
     long_description='',
-    install_requires=['numpy', 'scipy', 'argparse', 'matplotlib', 'wheel'],
+    install_requires=['numpy', 'scipy', 'argparse', 'matplotlib', 'wheel', 'rich'],
     packages = ["pysurfaceopt"],
     package_dir = {"pysurfaceopt": "pysurfaceopt"},
     package_data={'pysurfaceopt': ['data/*', 'data/ncsx/*']},
