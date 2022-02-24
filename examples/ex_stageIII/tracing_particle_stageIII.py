df264706adcc9aa0cabeefce86f9633504dfb1f7
Andrew Giuliani <giuliani@cims.nyu.edu>
2022-02-24 15:30:51 -0500

    edits to scaling

--- examples/ex_stageIII/tracing_particle_stageIII.py
+++ examples/ex_stageIII/tracing_particle_stageIII.py
@@ -59,34 +59,17 @@ os.makedirs(OUT_DIR, exist_ok=True)
 length=18
 assert length==18 or length==20 or length==22 or length==24
 ts_dict={18:'1639707710.6463501', 20: '1639796640.5101252', 22:'1642522947.7884622', 24:'1642523475.5701194'}
-boozer_surface_list, base_curves, base_currents, coils = pys.load_surfaces_in_stageII(mpol=10, ntor=10, stellsym=True, Nt_coils=16, idx_surfaces=[0,2,4], exact=True, length=length, time_stamp=ts_dict[length])
+boozer_surface_list, base_curves, base_currents, coils = pys.load_surfaces_in_stageII(mpol=10, ntor=10, stellsym=True, Nt_coils=16, idx_surfaces=[2,4], exact=True, length=length, time_stamp=ts_dict[length])
 bs_tf_list = [BiotSavart(coils) for bs in boozer_surface_list]
 J_toroidal_flux = [ToroidalFlux(boozer_surface, bs_tf) for boozer_surface, bs_tf in zip(boozer_surface_list, bs_tf_list)]
 J_aspect_ratio = [Aspect_ratio(boozer_surface.surface) for boozer_surface in boozer_surface_list]
 tf_list=[abs(tf.J()) for tf in J_toroidal_flux]
 ar_list=[ar.J() for ar in J_aspect_ratio]
-#max_tf = max(tf_list)
-#print("tf:",tf_list/max_tf)
-#print("ar:",ar_list)
-
-print("compute a surface close to the magnetic axis to normalize the field")
-boozer_surface_inner = boozer_surface_list[0]
-tf_spawn = J_toroidal_flux[0]
-iota0=boozer_surface_inner.res['iota']
-G0=boozer_surface_inner.res['G']
-boozer_surface_inner.targetlabel = 1e-3
-boozer_surface_inner.recompute_bell()
-tf_spawn.recompute_bell()
-res = boozer_surface_inner.solve_residual_equation_exactly_newton(tol=1e-13, maxiter=30,iota=iota0,G=G0)
-tf_list=[abs(tf.J()) for tf in J_toroidal_flux]
-ar_list=[ar.J() for ar in J_aspect_ratio]
-print("tf:",tf_list)
-print("ar:",ar_list)
 
 print("compute the 0.25 surface")
 # compute the s=0.25 surface
 vol_dict={18:-0.14036}
-boozer_surface_spawn = boozer_surface_list[1]
+boozer_surface_spawn = boozer_surface_list[0]
 tf_spawn = J_toroidal_flux[1]
 iota0=boozer_surface_spawn.res['iota']
 G0=boozer_surface_spawn.res['G']
@@ -100,7 +83,7 @@ max_tf = max(tf_list)
 print("new max tf",max_tf)
 print("tf:",tf_list/max_tf)
 print("ar:",ar_list)
-boozer_surface_outer=boozer_surface_list[2]
+boozer_surface_outer=boozer_surface_list[1]
 
 # convert these surfaces to full period surfaces
 mpol = 10
@@ -109,16 +92,13 @@ stellsym = True
 nfp = 2
 phis = np.linspace(0, 1, nfp*2*ntor+1, endpoint=False)
 thetas = np.linspace(0, 1, 2*mpol+1, endpoint=False)
-s_inner = SurfaceXYZTensorFourier(mpol=mpol, ntor=ntor, stellsym=stellsym, nfp=nfp, quadpoints_phi=phis, quadpoints_theta=thetas)
 s_spawn = SurfaceXYZTensorFourier(mpol=mpol, ntor=ntor, stellsym=stellsym, nfp=nfp, quadpoints_phi=phis, quadpoints_theta=thetas)
 s_outer = SurfaceXYZTensorFourier(mpol=mpol, ntor=ntor, stellsym=stellsym, nfp=nfp, quadpoints_phi=phis, quadpoints_theta=thetas)
 
-minor_radius_outer = minor_radius(s_outer)
-s_inner.x=boozer_surface_inner.surface.x*1.7/minor_radius_outer
+minor_radius_outer = minor_radius(boozer_surface_outer.surface)
 s_spawn.x=boozer_surface_spawn.surface.x*1.7/minor_radius_outer
 s_outer.x=boozer_surface_outer.surface.x*1.7/minor_radius_outer
 
-s_inner.to_vtk(OUT_DIR+"inner_surface")
 s_spawn.to_vtk(OUT_DIR+"spawn_surface")
 s_outer.to_vtk(OUT_DIR+"outer_surface")
 
@@ -128,8 +108,8 @@ print("Running 1_Simple/tracing_particle.py")
 print("====================================")
 # check whether we're in CI, in that case we make the run a bit cheaper
 ci = "CI" in os.environ and os.environ['CI'].lower() in ['1', 'true']
-nparticles = 3 if ci else 10
-degree = 2 if ci else 3
+nparticles = 100
+degree = 5
 
 """
 This examples demonstrate how to use SIMSOPT to compute guiding center
@@ -139,7 +119,7 @@ for c in base_curves:
     c.x=c.x*1.7/minor_radius_outer
 
 bs = BiotSavart(coils)
-bs.set_points(s_inner.gamma().reshape(-1,3))
+bs.set_points(s_outer.gamma().reshape(-1,3))
 Bfield=bs.B()
 modB = np.linalg.norm(Bfield, axis=-1)
 avgB = np.mean(modB)
@@ -148,13 +128,13 @@ for curr in base_currents:
 
 
 bs = BiotSavart(coils)
-bs.set_points(s_inner.gamma().reshape(-1,3))
+bs.set_points(s_outer.gamma().reshape(-1,3))
 Bfield=bs.B()
 modB = np.linalg.norm(Bfield, axis=-1)
 avgB = np.mean(modB)
 
-print("Mean(|B|) on surface close to axis =", avgB)
-print("Mean(surface close to axis radius) =", np.mean(np.linalg.norm(s_inner.gamma(), axis=-1)))
+print("Mean(|B|) on outer surface =", avgB)
+print("Minor radius of outer surface =", minor_radius(s_outer))
 
 sc_particle = SurfaceClassifier(s_outer, h=0.1, p=2)
 n = 32
@@ -174,7 +154,7 @@ def trace_particles(bfield, label, mode='gc_vac'):
     t1 = time.time()
     phis = [(i/4)*(2*np.pi/nfp) for i in range(4)]
     gc_tys, gc_phi_hits = trace_particles_starting_on_surface(
-        s_inner, bfield, nparticles, tmax=1e-3, seed=1, mass=ALPHA_PARTICLE_MASS, charge=ALPHA_PARTICLE_CHARGE,
+        s_spawn, bfield, nparticles, tmax=1e-3, seed=1, mass=ALPHA_PARTICLE_MASS, charge=ALPHA_PARTICLE_CHARGE,
         Ekin=3.5e6*ONE_EV, umin=-1, umax=+1, comm=comm,
         phis=phis, tol=1e-10,
         stopping_criteria=[LevelsetStoppingCriterion(sc_particle.dist)], mode=mode,
